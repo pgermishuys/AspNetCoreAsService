4708cf48f38f521c64dadfc3d652b3e6044c1725
Pieter Germishuys <pieter@pieterg.com>
2019-12-03 08:08:52 +0100

    Add sample

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.WindowsServices;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace WebHostAsService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var isService = !(Debugger.IsAttached || args.Contains("--console"));
+            var pathToContentRoot = Directory.GetCurrentDirectory();
+            var webHostArgs = args.Where(arg => arg != "--console").ToArray();
+ 
+            if (isService)
+            {
+                var pathToExe = Process.GetCurrentProcess().MainModule.FileName;
+                pathToContentRoot = Path.GetDirectoryName(pathToExe);
+            }
+ 
+            var host = WebHost.CreateDefaultBuilder(webHostArgs)
+                .UseContentRoot(pathToContentRoot)
+                .UseStartup<Startup>()
+                .Build();
+ 
+            if (isService)
+            {
+                host.RunAsService();
+            }
+            else
+            {
+                host.Run();
+            }
+        }
+    }
+}
